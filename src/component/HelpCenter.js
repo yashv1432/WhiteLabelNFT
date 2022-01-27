6029d7ef571ab7d8877ae2fe76930cbf3f021101
shivani soni <kajalsoni1845@gmail.com>
2022-01-27 19:48:55 +0530

    2nd push

--- src/component/HelpCenter.js
+++ src/component/HelpCenter.js
@@ -14,7 +14,7 @@ function HelpCenter() {
         <div class="row">
           <div class="col-sm-6 col-lg-3 col-md-5 col-12 p-5">
             <div
-              className="card"
+              className="card shadow"
               style={{
                 cursor: "pointer",
                 borderRadius: "10px",
@@ -22,16 +22,22 @@ function HelpCenter() {
               }}
             >
               <div class="card-body">
-                <img src={buying} />
+                <img
+                  src={buying}
+                  style={{ width: "50px", marginLeft: "-8px" }}
+                />
                 <Link
                   to="/Buying"
                   style={{ textDecoration: "none", color: "black" }}
                 >
-                  <h5 class="card-title" style={{ marginBottom: "0.5rem" }}>
+                  <h5
+                    class="card-title font-16 font-weight-700"
+                    style={{ marginBottom: "0.5rem", marginTop: "0.5rem" }}
+                  >
                     Buying
                   </h5>
                 </Link>
-                <p class="card-text">
+                <p class="card-text font-14">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor
                 </p>
@@ -40,13 +46,31 @@ function HelpCenter() {
           </div>
 
           <div class="col-md-5 col-lg-3 col-sm-6 col-11 p-5">
-            <div class="card" style={{ cursor: "pointer" }}>
+            <div
+              class="card shadow"
+              style={{
+                cursor: "pointer",
+                borderRadius: "10px",
+                width: "280px",
+              }}
+            >
               <div class="card-body">
-                <img src={selling} />
-                <h5 class="card-title" style={{ marginBottom: "0.5rem" }}>
-                  <Link to="/Fixed_Price">Selling</Link>
+                <img
+                  src={selling}
+                  style={{ width: "60px", marginLeft: "-8px" }}
+                />
+                <h5
+                  class="card-title font-16 font-weight-700"
+                  style={{ marginBottom: "0.5rem", marginTop: "0.5rem" }}
+                >
+                  <Link
+                    to="/Fixed_Price"
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    Selling
+                  </Link>
                 </h5>
-                <p class="card-text">
+                <p class="card-text font-14">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor
                 </p>
@@ -55,13 +79,31 @@ function HelpCenter() {
           </div>
 
           <div class="col-md-5 col-lg-3 col-sm-6 col-11 p-5">
-            <div class="card" style={{ cursor: "pointer" }}>
+            <div
+              class="card shadow"
+              style={{
+                cursor: "pointer",
+                borderRadius: "10px",
+                width: "280px",
+              }}
+            >
               <div class="card-body">
-                <img src={creating} />
-                <h5 class="card-title" style={{ marginBottom: "0.5rem" }}>
-                  <Link to="/Highest_Bid">Creating Collection</Link>
+                <img
+                  src={creating}
+                  style={{ width: "43px", marginLeft: "-8px" }}
+                />
+                <h5
+                  class="card-title font-16 font-weight-700"
+                  style={{ marginBottom: "0.5rem", marginTop: "0.5rem" }}
+                >
+                  <Link
+                    to="/Highest_Bid"
+                    style={{ textDecoration: "none", color: "#000" }}
+                  >
+                    Creating Collection
+                  </Link>
                 </h5>
-                <p class="card-text">
+                <p class="card-text font-14">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor
                 </p>
@@ -70,13 +112,26 @@ function HelpCenter() {
           </div>
 
           <div class="col-md-5 col-lg-3 col-sm-6 col-11 p-5">
-            <div class="card" style={{ cursor: "pointer" }}>
+            <div
+              class="card shadow"
+              style={{
+                cursor: "pointer",
+                borderRadius: "10px",
+                width: "280px",
+              }}
+            >
               <div class="card-body">
-                <img src={adding} />
-                <h5 class="card-title" style={{ marginBottom: "0.5rem" }}>
+                <img
+                  src={adding}
+                  style={{ width: "44px", marginLeft: "-8px" }}
+                />
+                <h5
+                  class="card-title font-16 font-weight-700"
+                  style={{ marginBottom: "0.5rem", marginTop: "0.5rem" }}
+                >
                   Adding NFT's
                 </h5>
-                <p class="card-text">
+                <p class="card-text font-14">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor
                 </p>
